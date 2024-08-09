dc1f8be29bad72403d4efa464b19589c3d7271e1
Morlok <nikitaillarionov2018@gmail.com>
2024-08-09 16:51:01 +0300

    changing info in db

--- registration/config/db.js
+++ registration/config/db.js
@@ -4,7 +4,7 @@ const connection = mysql.createConnection({
   host: 'localhost',
   port: 3307,
   user: 'root',
-  password: 'password',
+  password: 'Water2021',
   database: 'registration_database'
 });
 
