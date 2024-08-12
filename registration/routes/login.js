dd0c426a1a318aa5d4d66c7d5634ec494afb8604
Morlok <nikitaillarionov2018@gmail.com>
2024-08-12 17:03:05 +0300

    still needs rework

--- registration/routes/login.js
+++ registration/routes/login.js
@@ -10,33 +10,33 @@ router.get('/login.html', (req, res) => {
 });
 
 router.post('/login.html', validateLogin, loginUser);
-router.post('/login.html', (req, res) => {
-  const { email, password } = req.body;
+// router.post('/login.html', (req, res) => {
+//   const { email, password } = req.body;
 
-  const query = 'SELECT * FROM users WHERE email = ?';
-  db.query(query, [email], async (err, results) => {
-    if (err) throw err;
+//   const query = 'SELECT * FROM users WHERE email = ?';
+//   db.query(query, [email], async (err, results) => {
+//     if (err) throw err;
 
-    if (results.length === 0) {
-      return res.status(400).send('User not found.');
-    }
+//     if (results.length === 0) {
+//       return res.status(400).send('User not found.');
+//     }
 
-    const user = results[0];
-    const isValid = await bcrypt.compare(password, user.password);
+//     const user = results[0];
+//     const isValid = await bcrypt.compare(password, user.password);
 
-    if (!isValid) {
-      return res.status(400).send('Invalid credentials.');
-    }
+//     if (!isValid) {
+//       return res.status(400).send('Invalid credentials.');
+//     }
 
     
-    req.session.userId = user.id;
-    req.session.loggedin = true;
+//     req.session.userId = user.id;
+//     req.session.loggedin = true;
 
-    req.session.cookie.maxAge = 60 * 1000; // Exactly 1 minute
+//     req.session.cookie.maxAge = 60 * 1000; // Exactly 1 minute
 
-    res.redirect('/home.html');
-  });
-});
+//     res.redirect('/home.html');
+//   });
+// });
 
 module.exports = router;
 
